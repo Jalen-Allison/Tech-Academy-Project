6fbbb0675824eba830974cf100f037dd27f1127b
Jalen-Allison <jalenallison21@yahoo.com>
2022-03-04 15:36:08 -0800

    Expressions and functions

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main1.js
@@ -0,0 +1,16 @@
+       window.alert("Hello, World!");
+
+
+       document.write("\"Be who you are and say what you feel."
+       + "Because those who mind don\'t matter and those who matter don\'t mind.\""
+       +"-Dr. Seuss"); //Writing a concatenated string by using double quotes and back slashes w/ +signs
+      
+       var A = "Hello, I'm Jalen";
+       var B = "- Jalen";
+       document.write(A+B) //Concatenating two variables
+
+       var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+            Daughter = "Penny", Son = "Zorro";
+    document.write(Dad); //Writing multiple variables 
+
+    document.write(3+3); //Writing an expression
---
+++ Basic JavaScript Projects/Project2_functions/JS/main2.js
@@ -0,0 +1,8 @@
+function My_First_Function() { //Defining a function and naming it
+    var str = "This is the button text!"; //Defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str; //Putting the value of the variable into the HTML elementFromPoint
+                                                            //with the "BUtton_Text" id
+}
+
+var str = new String("Font color Text");
+document.write(str.fontcolor( "blue" ));
