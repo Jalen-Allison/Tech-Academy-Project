93c48acc571b5016f54c131688ffa5c28c1482b0
Jalen-Allison <jalenallison21@yahoo.com>
2022-03-03 20:52:05 -0800

    JavaScript assignments

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+       window.alert("Hello, World!");
+
+
+       document.write("\"Be who you are and say what you feel."
+       + "brcause those who mind don\'t matter and those who matter don\'t mind.\""
+       +"-Dr. Seuss"); //Writing a concatenated string by using double quotes and back slashes w/ +signs
+
+       var B = "Hello, I'm Jalen" + " - Jalen"
+       document.write(B)
+
+       var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+            Daughter = "Penny", Son = "Zorro";
+    document.write(Dad); //Writing multiple variables 
+
+    document.write(3+3); //Writing an expression
+
+    function My_First_Function() { //Defining a function and naming it
+        var str = "This is the button text!"; //Defining a variable and giving it a string value
+        document.getElementById("Button_Text").innerHTML = str; //Putting the value of the variable into the HTML elementFromPoint
+                                                                //with the "BUtton_Text" id
+    }
+
+    var str = new String("Font color Text");
+    document.write(str.fontcolor( "blue" ));
